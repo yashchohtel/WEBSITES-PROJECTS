bd0e792ed9dddf5d33308b8014cc695602334e9d
“yashchohtel” <yashchohtel@gmail.com>
2024-02-28 20:18:53 +0530

    done

---
+++ 9. FRESHMEAL - grosry store/js/script.js
@@ -0,0 +1,11 @@
+// SCRIPT FOR SLIDER
+
+$(document).ready(function(){
+    
+    $('.nav_trigger').click(function(){
+        $('._site_content_wrapper').toggleClass('scaled')
+    })
+
+});
+
+// FOR NAVIGATION 
