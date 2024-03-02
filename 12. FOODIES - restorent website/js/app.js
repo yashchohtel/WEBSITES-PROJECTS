e3d6386de630407c9cfb310da2bdf938a6ad53f8
“yashchohtel” <yashchohtel@gmail.com>
2024-03-02 22:27:42 +0530

    done

---
+++ 12. FOODIES - restorent website/js/app.js
@@ -0,0 +1,46 @@
+// FOR ACTIVE NAVIGATION VAR 
+
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on")
+    } else{
+        nav.classList.remove("scroll-on")
+    }
+}
+
+// FOR HIEIND NAVIGATION 
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show")
+    })
+})
+
+
+/* FOR COUNTER SECTION */
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+
+  counter("count1", 0, 1287, 3000);
+  counter("count2", 100, 1224, 3000);
+  counter("count3", 0, 4287, 3000);
+  counter("count4", 0, 2457, 3000);
+});
+
+
