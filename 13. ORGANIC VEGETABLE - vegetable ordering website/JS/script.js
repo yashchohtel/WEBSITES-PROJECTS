beb572f4f3f035fe71f8af4b669ede8927186ea9
“yashchohtel” <yashchohtel@gmail.com>
2024-03-03 21:49:20 +0530

    done

---
+++ 13. ORGANIC VEGETABLE - vegetable ordering website/JS/script.js
@@ -0,0 +1,20 @@
+// header scroll
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 50) {
+    nav.classList.add("header_scrolled");
+  } else{
+    nav.classList.remove("header_scrolled");
+  }
+};
+
+// nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(e){
+    e.addEventListener("click", function(){
+        navCollapse.classList.remove("show")
+    })
+})
